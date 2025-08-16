044fd81fd6073f5026bfc3ae495f60f4a7f7ae6c
ILMAN <165402049+ilman66666@users.noreply.github.com>
2025-08-16 22:09:46 +0700

    Delete src directory

--- src/declarations/auth/auth.did.js
+++
@@ -1,7 +0,0 @@
-export const idlFactory = ({ IDL }) => {
-  return IDL.Service({
-    'hasAccess' : IDL.Func([IDL.Principal, IDL.Text], [IDL.Bool], ['query']),
-    'subscribeUser' : IDL.Func([IDL.Principal, IDL.Vec(IDL.Text)], [], []),
-  });
-};
-export const init = ({ IDL }) => { return []; };
